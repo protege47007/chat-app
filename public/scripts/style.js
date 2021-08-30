5ba958b91969d3e0e4fa98de5a3a67cc5de7ad6d
Adewumi David <Davidak.a@yahoo.com>
2021-08-30 20:57:12 +0100

    complete admin ui

--- public/scripts/style.js
+++ public/scripts/style.js
@@ -1,3 +1,4 @@
+//contact search bar animation
 const searchBtn = document.querySelector('#contactSearchBtn');
 const searchField = document.querySelector('#contactSearchField');
 
@@ -33,3 +34,37 @@ searchField.addEventListener('blur', ()=>{
         }
     }, 500);
 }, true);
+
+
+
+//message input tab (rich-text)custom paste function for editable html content
+const editorEle = document.getElementById('message');
+
+// Handle the `paste` event
+editorEle.addEventListener('paste', function(e) {
+    // Prevent the default action
+    e.preventDefault();
+
+    // Get the copied text from the clipboard
+    const text = (e.clipboardData)
+        ? (e.originalEvent || e).clipboardData.getData('text/plain')
+        // For IE
+        : (window.clipboardData ? window.clipboardData.getData('Text') : '');
+    
+    if (document.queryCommandSupported('insertText')) {
+        document.execCommand('insertText', false, text);
+    } else {
+        // Insert text at the current position of caret
+        const range = document.getSelection().getRangeAt(0);
+        range.deleteContents();
+
+        const textNode = document.createTextNode(text);
+        range.insertNode(textNode);
+        range.selectNodeContents(textNode);
+        range.collapse(false);
+
+        const selection = window.getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+});
